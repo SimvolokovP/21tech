3e68ce763fd72c02f58eca7d5a5a1fc2dfd18ba2
Simvolokov Pavel <psimvolokov@gmail.com>
2025-07-09 15:46:46 +0300

    add active nav link

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -1,29 +1,43 @@
-import { Link } from '@/i18n/navigation'
-import { useTranslations } from 'next-intl'
+import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
+import { usePathname } from "next/navigation";
+import { match } from "path-to-regexp";
 
 export function DesktopNav() {
-	const t = useTranslations('Header')
+  const t = useTranslations("Header");
+  const pathname = usePathname();
 
-	const navigation = [
-		{ text: t('navigation.services'), href: '/services' },
-		{ text: t('navigation.cases'), href: '/cases' },
-		{ text: t('navigation.solutions'), href: '/solutions' },
-		{ text: t('navigation.contacts'), href: '/contact-us' },
-	]
+  const navigation = [
+    { text: t("navigation.services"), href: "/services" },
+    { text: t("navigation.cases"), href: "/cases" },
+    { text: t("navigation.solutions"), href: "/solutions" },
+    { text: t("navigation.contacts"), href: "/contact-us" },
+  ];
 
-	return (
-		<nav className='hidden md:flex flex-1 justify-center'>
-			<ul className='flex space-x-8 text-gray-700 font-medium'>
-				{navigation.map((navLink, index) => (
-					<Link key={index} href={`/${navLink.href}`}>
-						<li>
-							<span className='hover:text-black transition-colors text-[14px] cursor-pointer'>
-								{navLink.text}
-							</span>
-						</li>
-					</Link>
-				))}
-			</ul>
-		</nav>
-	)
+  return (
+    <nav className="hidden md:flex flex-1 justify-center">
+      <ul className="flex space-x-8 text-gray-700 font-medium">
+        {navigation.map((navLink, index) => {
+          const matcher = match(`/:locale${navLink.href}`, {
+            decode: decodeURIComponent,
+          });
+          const isActive = matcher(pathname);
+
+          return (
+            <li key={index}>
+              <Link href={navLink.href}>
+                <span
+                  className={`${
+                    isActive ? "text-black" : ""
+                  } hover:text-black transition-colors text-[14px] cursor-pointer`}
+                >
+                  {navLink.text}
+                </span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
